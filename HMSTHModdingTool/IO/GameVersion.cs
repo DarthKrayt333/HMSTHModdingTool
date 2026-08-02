e50604b49b814be9fef816235459fd0ee76a00d4
DarthKrayt333 <blvgs999@gmail.com>
2026-08-02 03:06:07 +0200

    Add GameVersion enum for Harvest Moon versions

---
+++ HMSTHModdingTool/IO/GameVersion.cs
@@ -0,0 +1,29 @@
+namespace HMSTHModdingTool
+{
+    /// <summary>
+    ///     Identifies which version of
+    ///     Harvest Moon: Save The Homeland
+    ///     is being worked with.
+    /// </summary>
+    public enum GameVersion
+    {
+        /// <summary>
+        ///     USA release: SLUS_202.51
+        /// </summary>
+        USA = 0,
+
+        /// <summary>
+        ///     Japanese release:
+        ///     SLPS_201.04
+        /// </summary>
+        JAP = 1,
+
+        /// <summary>
+        ///     Japanese Demo release:
+        ///     SLPM_601.47
+        ///     (Bokujou Monogatari 3
+        ///     Taikenban)
+        /// </summary>
+        DEMO = 2
+    }
+}
