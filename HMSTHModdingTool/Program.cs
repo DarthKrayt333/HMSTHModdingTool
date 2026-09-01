522c26e486858d15e613630e6e25eafd4ea3aebf
DarthKrayt333 <blvgs999@gmail.com>
2026-09-01 22:37:04 +0200

    Update Program.cs

--- HMSTHModdingTool/Program.cs
+++ HMSTHModdingTool/Program.cs
@@ -21,7 +21,7 @@ namespace HMSTHModdingTool
             "HMSTHModdingTool original as" +
             " HDATextTool by gdkchan";
         const string TOOL_VERSION =
-            "v1.5.3-Beta";
+            "v1.5.5-Beta";
         const string TOOL_AUTHOR =
             "gdkchan + DarthKrayt333" +
             " & HMSTH Community";
